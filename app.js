189155e5632753f6a06ce4fe71bbe1bedeceb4c3
Kabishan <suvendrk@mcmaster.ca>
2020-05-08 20:06:12 -0400

    Significant progress made. Can now create, delete, clear and filter tasks

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ eventListeners();
 
 function eventListeners() {
   form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
-  if (taskInput.value === '') alert('Add a task');
+  if (taskInput.value === '' || isDuplicate(taskInput.value))
+    alert('Please add a valid task (no duplicates or empty tasks)');
   else {
     const li = document.createElement('li');
     const link = document.createElement('a');
@@ -28,4 +32,42 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+function isDuplicate(text) {
+  let duplicate = false;
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    if (task.firstChild.textContent === text) duplicate = true;
+  });
+
+  return duplicate;
+}
+
+function removeTask(e) {
+  const deleteItem = e.target.parentElement;
+  const task = deleteItem.parentElement;
+  const text = task.firstChild.textContent;
+
+  if (
+    deleteItem.classList.contains('delete-item') &&
+    confirm(`Are you sure you want to delete task "${text}"?`)
+  )
+    task.remove();
+}
+
+// Using a while loop is faster than taskList.innerHTML = '';
+function clearTasks(e) {
+  while (taskList.firstChild) taskList.removeChild(taskList.firstChild);
+  e.preventDefault();
+}
+
+function filterTasks(e) {
+  const filter = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(filter) != -1) task.style.display = 'block';
+    else task.style.display = 'none';
+  });
 }
